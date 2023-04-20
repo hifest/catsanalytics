8ade1a825724d88868446d21396b4877ccdd7523
Maxym <maximsench@gmail.com>
2023-04-20 12:38:52 +0300

    hui

--- src/tools/btnGenerator.js
+++ src/tools/btnGenerator.js
@@ -1,14 +1,14 @@
-function ObjectGenerator({ objects }) {
-  return (
-    <div>
-      {/* {objects.map((object) => (
-        <button 
-            className={objects.class}  
-            onClick={() => {objects.click}}
-            >Зареєструватись</button>
-      ))} */}
-    </div>
-  );
-}
+// function ObjectGenerator({ objects }) {
+//   return (
+//     <div>
+//       {objects.map((object) => (
+//         <button 
+//             className={objects.class}  
+//             onClick={() => {objects.click}}
+//             >Зареєструватись</button>
+//       ))}
+//     </div>
+//   );
+// }
 
-export default ObjectGenerator
+// export default ObjectGenerator
